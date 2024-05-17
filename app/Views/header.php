89d3ac0532e4df73efdecec233a817fe8911c5dd
Benjinho2 <francooviedofb@gmail.com>
2024-05-17 09:53:12 -0300

    asd121

---
+++ app/Views/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Sitio Web</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Asegúrate de ajustar el path según sea necesario -->
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a Mi Sitio Web</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="about.php">Acerca de</a></li>
+                <li><a href="contact.php">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="estilo/style.css">
-    <div class="mano">
-    </div>
+    <img src="imagenes/arboles.avif" alt="">
 </head>
 <body>
+<?php include 'header.php'; ?>
     <h1>ABOT</h1>
     <a href="autenticacion/login">login</a>
     <a href="autenticacion/register">register</a>
+<?php include 'footer.php'; ?>
 </body>
 </html>
