b4d27a8bbb95c2b43def89db89d2b485fadca8c8
Naahuuel <nahuelalvarez@alumnos.itr3.edu.ar>
2024-05-22 10:41:22 -0300

    22.05 10:40
    Add db UsuarioModel y Controlador

--- app/Controllers/CAutenticacion.php
+++ app/Controllers/CAutenticacion.php
@@ -15,19 +15,14 @@ class CAutenticacion extends BaseController
     }
 
     
-    public function auth()
+    public function registro()
     {
-        $username = $this->request->getPost('username');
-        $password = $this->request->getPost('password');
+        
+    }
 
-        // Aquí deberías validar las credenciales (por ejemplo, consultando una base de datos)
-        // Este es solo un ejemplo básico
-        if ($username === 'admin' && $password === 'password') {
-            // Usuario autenticado, redirigir a la página de inicio o a otra página
-            return redirect()->to('/home');
-        } else {
-            // Credenciales incorrectas, mostrar mensaje de error
-            return view('login', ['error' => 'Username or password is incorrect.']);
-        }
+
+    public function logueo()
+    {
+
     }
 }
