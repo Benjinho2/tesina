61adba039ea6fad4cbec549bf18b76cb9a02ddc8
Naahuuel <nahuelalvarez@alumnos.itr3.edu.ar>
2024-06-14 01:22:11 -0300

    header
    item desplegable responsive

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url('estilo/header.css'); ?>">   
-    <link rel="stylesheet" href="<?= base_url('estilo/fuente.css'); ?>">   
-    <title>Header</title> 
+    <link rel="stylesheet" href="<?= base_url('estilo/header.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('estilo/fuente.css'); ?>">
+    <title>Header</title>
 </head>
 <body>
 
 <header>
     <div class="container">
         <a href="<?= base_url('/'); ?>" class="logo">
-        <img src="<?= base_url('imagenes/planta.png'); ?>" ><p>AquaBot</p></a>
+            <img src="<?= base_url('imagenes/planta.png'); ?>"><p>AquaBot</p></a>
+
+        <input type="checkbox" id="menu-toggle" class="menu-button">
+        <label for="menu-toggle" class="menu-button"></label>
 
         <nav>
             <ul>
@@ -23,11 +26,18 @@
                 <?php else: ?>
                 <li><a href="<?= site_url('autenticacion/login'); ?>">Login</a></li>
                 <?php endif; ?>
-
             </ul>
         </nav>
     </div>
 </header>
+
+
+<script>
+    document.getElementById('menu-toggle').addEventListener('click', function() {
+        const navMenu = document.querySelector('nav ul');
+        navMenu.classList.toggle('open');
+    });
+</script>
 
 </body>
 </html>
